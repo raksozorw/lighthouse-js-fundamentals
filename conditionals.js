e39ef87ca508cd5ca8beef95b3b53cb6e3bfdbf9
Oskar Wroz <oskarwroz@gmail.com>
2020-02-24 18:20:26 +0000

    more examples

--- conditionals.js
+++ conditionals.js
@@ -8,3 +8,12 @@ if (temperature < 0) {
   console.log("shorts ok")
 }
 console.log("you good to go")
+
+const isCitizen = true 
+const age = 26
+
+if (isCitizen && age >18){
+  console.log("You can vote")
+}else {
+  console.log("you cannot vote")
+}
